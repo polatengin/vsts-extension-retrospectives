80c5696248afaf73f6ceedb2372f2b765b69041e
polatengin <enpolat@microsoft.com>
2020-12-17 22:23:11 +0300

    console log calls removed

--- RetrospectiveExtension.Frontend/dal/itemDataService.tsx
+++ RetrospectiveExtension.Frontend/dal/itemDataService.tsx
@@ -128,7 +128,7 @@ class ItemDataService {
     const feedbackItem: IFeedbackItemDocument = await this.getFeedbackItem(boardId, feedbackItemId);
 
     if (!feedbackItem) {
-      console.log(`Cannot increment upvote for a non-existent feedback item. Board: ${boardId}, Item: ${feedbackItemId}`);
+      //`Cannot increment upvote for a non-existent feedback item. Board: ${boardId}, Item: ${feedbackItemId}`
       return undefined;
     }
 
@@ -145,7 +145,7 @@ class ItemDataService {
     const feedbackItem: IFeedbackItemDocument = await this.getFeedbackItem(boardId, feedbackItemId);
 
     if (!feedbackItem) {
-      console.log(`Cannot update title for a non-existent feedback item. Board: ${boardId}, Item: ${feedbackItemId}`);
+      //`Cannot update title for a non-existent feedback item. Board: ${boardId}, Item: ${feedbackItemId}`
       return undefined;
     }
 
